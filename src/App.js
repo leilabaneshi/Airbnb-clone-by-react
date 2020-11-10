4cb70c9e519c9f14fa07a8a05ac0da2f6cfb2141
Leila Baneshi <baneshi.leila@gmail.com>
2020-11-10 16:18:54 +0100

    setup react-router

--- src/App.js
+++ src/App.js
@@ -3,22 +3,38 @@ import './App.css';
 import Header from './Header';
 import Home from './Home';
 import Footer from './Footer';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+import SearchPage from './SearchPage';
 
 
 function App() {
   return (
     <div className="app">
+      <Router>
+        <Header />
 
+        <Switch>
+          <Route path='/Search'>
+            <SearchPage />
+          </Route>
+          <Route path='/'>
+            <Home />
+          </Route>
+        </Switch>
+        <Footer />
+      </Router>
       {/**HOME */}
-      <Header />
-      <Home />
-      <Footer />
       {/**header */}
       {/**banner */}
       {/**search */}
       {/**cart */}
       {/**footer */}
       {/**search page */}
+
     </div>
   );
 }
---
+++ src/SearchPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './SearchPage.css';
+
+
+function SearchPage() {
+    return (
+        <div>
+            <p>search</p>
+        </div>
+    )
+}
+
+export default SearchPage
